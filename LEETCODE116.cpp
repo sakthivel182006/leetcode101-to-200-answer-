9f88cca15ea6998c9ff5e603216647d03aafd6a8
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2024-02-04 17:41:16 +0530

    leetcode116
    leetcode116 answer

---
+++ LEETCODE116.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) {
+            return root;
+        }
+        queue<Node*> q{ {root} };
+        while (!q.empty()) {
+            Node* p = nullptr;
+            for (int n = q.size(); n; --n) {
+                Node* node = q.front();
+                q.pop();
+                if (p) {
+                    p->next = node;
+                }
+                p = node;
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return root;
+    }
+};
