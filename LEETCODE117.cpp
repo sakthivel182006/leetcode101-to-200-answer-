556ea539b7b903165922f54795bb2102cfbd5d14
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2024-02-12 23:02:09 +0530

    leetcode117
    leetcode117 answer

---
+++ LEETCODE117.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        Node* node = root;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+        auto modify = [&](Node* curr) {
+            if (!curr) {
+                return;
+            }
+            if (!next) {
+                next = curr;
+            }
+            if (prev) {
+                prev->next = curr;
+            }
+            prev = curr;
+        };
+        while (node) {
+            prev = next = nullptr;
+            while (node) {
+                modify(node->left);
+                modify(node->right);
+                node = node->next;
+            }
+            node = next;
+        }
+        return root;
+    }
+};
